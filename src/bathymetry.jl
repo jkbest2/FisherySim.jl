58877b4e922350b9e2ea77cd0b7d77ec99dd47e2
John Best <jkbest@gmail.com>
2017-12-05 11:25:59 -0800

    Add bathymetry types and methods.

---
+++ src/bathymetry.jl
@@ -0,0 +1,52 @@
+"""
+    expcov(dist, σ, ϕ)
+
+Exponential covariance kernel.
+"""
+function expcov(dist, σ, ϕ)
+    σ * exp(-dist / ϕ)
+end
+"""
+    sqexpcov(dist, σ, ϕ)
+
+Squared exponential covariance kernel.
+"""
+function sqexpcov(dist, σ, ϕ)
+    σ * exp(-dist^2 / 2 / ϕ)
+end
+
+"""
+    BathymetryModel{T<:AbstractMvNormal}
+        D::T
+        locs::Array{Float64, 2}
+        griddim::Vector{Float64}
+
+A multivariate normal with defined R^2 locations for generating
+bathymetry realizations
+"""
+struct BathymetryModel{T<:AbstractMvNormal} <: Any
+    D::T
+    locs::Array{Float64, 2}
+    griddim::Vector{Float64}
+end
+
+"""
+    Bathymetry
+        bathy::Vector{Float64}
+        loca::Array{Float64, 2}
+        griddim::Vector{Float64}
+
+A realization of a `BathymetryModel`. Typically created using the
+`rand(BM::BathyModel)` method.
+"""
+struct Bathymetry <: Any
+    bathy::Vector{Float64}
+    locs::Array{Float64, 2}
+    griddim::Vector{Float64}
+end
+
+function rand(BM::BathymetryModel)
+    bathy = rand(BM.D)
+    bathy .-= minimum(bathy)
+    Bathymetry(bathy, BM.locs, BM.griddim)
+end
