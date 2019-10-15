0ce1d7256b596a78d86704bac03dfc665abd3d8f
John Best <jkbest@gmail.com>
2019-10-15 13:56:56 -0700

    Clean up dependencies
    Remove Distances, StaticArrays, and StatsFuns. Also set TweedieDistributions
    >=v0.1.0.

--- src/FisherySim.jl
+++ src/FisherySim.jl
@@ -3,90 +3,102 @@ module FisherySim
 using Distributions
 using TweedieDistributions
 using StatsBase
-using StatsFuns         # For `logit` and `logistic`; worth it?
 using LinearAlgebra
 using Random
 using PDMats
 
-import Base: rand,
+import Base:
+    rand,
     +, -,
     step,
     sum,
     getindex, setindex!,
     size, length, eachindex,
     copy
-import StatsBase: sample, cov
-import Distributions: location
+import StatsBase:
+    sample,
+    cov
+import Distributions:
+    location
 
 include("fisherydomain.jl")
-export AbstractFisheryDomain,
-       DiscreteFisheryDomain,
-       GriddedFisheryDomain,
-       size,
-       length,
-       eachindex,
-       sample
-       ## Don't export:
-       ## calculate_distances,
-       ## map_symm
+export
+    AbstractFisheryDomain,
+    DiscreteFisheryDomain,
+    GriddedFisheryDomain,
+    size,
+    length,
+    eachindex,
+    sample
 
 include("covkernels.jl")
-export AbstractCovarianceKernel,
-       ExpCov,
-       Matérn32Cov,
-       Matern32Cov,
-       AR1,
-       cov
+export
+    AbstractCovarianceKernel,
+    ExpCov,
+    Matérn32Cov,
+    Matern32Cov,
+    AR1,
+    cov
 
 include("matrixlognormal.jl")
-export MatrixLogNormal,
-       location
+export
+    MatrixLogNormal,
+    location
 
 include("domaindistributions.jl")
-export DomainDistribution
+export
+    DomainDistribution
 
 include("bathymetry.jl")
-export BathymetryModel,
-       Bathymetry,
-       rand
+export
+    BathymetryModel,
+    Bathymetry,
+    rand
 
 include("pop_dynamics.jl")
-export PopulationDynamicsModel,
-       PopState,
-       Schaefer,
-       PellaTomlinson,
-       StochasticProduction,
-       vecstate,
-       step,
-       sum,
-       setindex!,
-       copy
+export
+    PopulationDynamicsModel,
+    PopState,
+    Schaefer,
+    PellaTomlinson,
+    StochasticProduction,
+    vecstate,
+    step,
+    sum,
+    setindex!,
+    copy
 
 include("movement.jl")
-export MovementModel, eqdist, approx_eqdist
+export
+    MovementModel,
+    eqdist,
+    approx_eqdist
 
 include("targeting.jl")
-export AbstractTargetingBehavior,
-       RandomTargeting,
-       FixedTargeting,
-       PreferentialTargeting,
-       target
+export
+    AbstractTargetingBehavior,
+    RandomTargeting,
+    FixedTargeting,
+    PreferentialTargeting,
+    target
 
 include("catchability.jl")
-export Catchability
+export
+    Catchability
 
 include("vessels.jl")
-export Vessel,
-       Catch,
-       logistic,
-       CPUE,
-       +, -,
-       fish!,
-       getindex,
-       Fleet,
-       vessels
+export
+    Vessel,
+    Catch,
+    CPUE,
+    +, -,
+    fish!,
+    getindex,
+    Fleet,
+    vessels
 
 include("simulation.jl")
-export simulate
+export
+    simulate
 
 end # module
