3d33a57d13f95c78704479cfc32199dd54ebd9cc
John Best <jkbest@gmail.com>
2019-12-01 22:47:52 -0800

    Add test that Arpack result approximates full result

--- test/test-movement.jl
+++ test/test-movement.jl
@@ -1,5 +1,6 @@
 @testset "Movement models" begin
     @test eqdist_ap isa PopState
+    @test eqdist_ap0.P ≈ eqdist_eigvecs
     @test all(sum(move.M; dims = 1) .≈ 1)
     @test all(0 .≤ eqdist_ap0.P .≤ 1)
     @test sum(eqdist_ap0) ≈ 1.0
