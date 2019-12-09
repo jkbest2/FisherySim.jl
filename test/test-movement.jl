9380ce1ef4a685c08ec6f6304097927d7f74615e
John Best <jkbest@gmail.com>
2019-12-09 15:15:12 -0800

    Make eqdist more robust
    Continue reducing tolerance on Arpack solver until real part of the first
    eigenvector is strictly positive.

--- test/test-movement.jl
+++ test/test-movement.jl
@@ -1,6 +1,7 @@
 @testset "Movement models" begin
     @test eqdist_ap isa PopState
     @test eqdist_ap0.P ≈ eqdist_eigvecs
+    @test all(eqdist_ap.P .> 0)
     @test all(sum(move.M; dims = 1) .≈ 1)
     @test all(0 .≤ eqdist_ap0.P .≤ 1)
     @test sum(eqdist_ap0) ≈ 1.0
